e3586e17d18ba003547e08dfafe3be80af29b159
Vitinho <vitorcbs18@gmail.com>
2024-08-27 21:22:00 -0300

    feat: Added color and shape options to button

--- projects/vicbrary/src/lib/button/button.component.ts
+++ projects/vicbrary/src/lib/button/button.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'lib-button',
+  selector: 'vby-button',
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
 
   @Input() label: string = ''
+  @Input() color: string = 'primary';
+  @Input() shape: 'rounded' | 'square' = 'rounded';   @Input() outlined: boolean = false;
 }
